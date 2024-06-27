c6a042fb6de2da9b3d577cc071e659806df15968
Prasanna <vjprasanna18@gmail.com>
2024-06-27 13:44:05 +0530

    bug fixed

--- app/_landing/logo.tsx
+++ app/_landing/logo.tsx
@@ -71,6 +71,7 @@ const Logo = () => (
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           {IMAGES.map((image) => (
             <img
+              key={image.id}
               className={image.className}
               src={image.src}
               alt="Logo"
